b7c2758456198cbfe12d82ab6dc96e8f86ac4afa
hafidi-alaoui <ayoub.hafidi.alaoui@hotmail.fr>
2022-10-06 20:23:45 +0200

    add delete and search

--- src/main/java/com/ayoub/patientsmvc/repositories/PatientRepository.java
+++ src/main/java/com/ayoub/patientsmvc/repositories/PatientRepository.java
@@ -1,7 +1,10 @@
 package com.ayoub.patientsmvc.repositories;
 
 import com.ayoub.patientsmvc.entities.Patient;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PatientRepository extends JpaRepository<Patient, Long> {
+    Page<Patient> findByNameContains(String keyWord, Pageable pageable);
 }
--- src/main/java/com/ayoub/patientsmvc/web/PatientController.java
+++ src/main/java/com/ayoub/patientsmvc/web/PatientController.java
@@ -8,21 +8,45 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @AllArgsConstructor
-@RequestMapping("/patient")
 public class PatientController {
     private PatientRepository patientRepository;
 
     @GetMapping("/index")
-    public String patients(Model model, @RequestParam(value = "page", defaultValue = "0") int page, @RequestParam(value = "size", defaultValue = "4") int size) {
-        Page<Patient> pagePatients = patientRepository.findAll(PageRequest.of(page, size));
+    public String patients(Model model,
+                           @RequestParam(value = "page", defaultValue = "0") int page,
+                           @RequestParam(value = "size", defaultValue = "4") int size,
+                           @RequestParam(name = "keyWord", defaultValue = "") String keyWord
+    ) {
+        Page<Patient> pagePatients = patientRepository.findByNameContains(keyWord, PageRequest.of(page, size));
         model.addAttribute("patientList", pagePatients);
         model.addAttribute("numberOfPages", new int[pagePatients.getTotalPages()]);
         model.addAttribute("currentPage", page);
+        model.addAttribute("keyWord", keyWord);
+        System.out.println(pagePatients.getTotalPages());
+        return "patients";
+    }
+
+    @GetMapping("/delete")
+    public String deletePatient(Long id, String keyWord, int page) {
+        patientRepository.deleteById(id);
+        return "redirect:/index?page=" + page + "&keyWord=" + keyWord;
+    }
+
+    @GetMapping("/")
+    public String getPatients(Model model,
+                              @RequestParam(value = "page", defaultValue = "0") int page,
+                              @RequestParam(value = "size", defaultValue = "4") int size,
+                              @RequestParam(name = "keyWord", defaultValue = "") String keyWord
+    ) {
+        Page<Patient> pagePatients = patientRepository.findByNameContains(keyWord, PageRequest.of(page, size));
+        model.addAttribute("patientList", pagePatients);
+        model.addAttribute("numberOfPages", new int[pagePatients.getTotalPages()]);
+        model.addAttribute("currentPage", page);
+        model.addAttribute("keyword", keyWord);
         System.out.println(pagePatients.getTotalPages());
         return "patients";
     }
